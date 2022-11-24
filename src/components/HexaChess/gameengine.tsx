7b5d032dbf5b1e56774b8b911c77e48810f3cc1e
Simon Krempin <67335158+simonkrempin@users.noreply.github.com>
2022-11-24 16:30:12 +0100

    two teams
    implemented the second team

--- src/components/HexaChess/gameengine.tsx
+++ src/components/HexaChess/gameengine.tsx
@@ -26,6 +26,25 @@ export const GameEngine = () => {
         { type: "pawn", color: "black", position: { q: 2, r: 1, s: -3 } },
         { type: "pawn", color: "black", position: { q: 3, r: 1, s: -4 } },
         { type: "pawn", color: "black", position: { q: 4, r: 1, s: -5 } },
+
+        { type: "king", color: "white", position: { q: -1, r: -4, s: 5 } },
+        { type: "queen", color: "white", position: { q: 1, r: -5, s: 4 } },
+        { type: "rook", color: "white", position: { q: 3, r: -5, s: -2 } },
+        { type: "rook", color: "white", position: { q: -3, r: -2, s: 5 } },
+        { type: "bishop", color: "white", position: { q: 0, r: -5, s: 5 } },
+        { type: "bishop", color: "white", position: { q: 0, r: -4, s: 4 } },
+        { type: "bishop", color: "white", position: { q: 0, r: -3, s: 3 } },
+        { type: "knight", color: "white", position: { q: -2, r: -3, s: 5 } },
+        { type: "knight", color: "white", position: { q: 2, r: -5, s: 3 } },
+        { type: "pawn", color: "white", position: { q: -4, r: -1, s: 5 } },
+        { type: "pawn", color: "white", position: { q: -3, r: -1, s: 4 } },
+        { type: "pawn", color: "white", position: { q: -2, r: -1, s: 3 } },
+        { type: "pawn", color: "white", position: { q: -1, r: -1, s: 2 } },
+        { type: "pawn", color: "white", position: { q: 0, r: -1, s: 1 } },
+        { type: "pawn", color: "white", position: { q: 1, r: -2, s: 1 } },
+        { type: "pawn", color: "white", position: { q: 2, r: -3, s: 1 } },
+        { type: "pawn", color: "white", position: { q: 3, r: -4, s: 1 } },
+        { type: "pawn", color: "white", position: { q: 4, r: -5, s: 1 } },
     ];
 
     return(
